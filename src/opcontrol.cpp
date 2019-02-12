0d00d634e5ad87b9b9529d0639b6b925728aa54d
wuyunze2005 <15067000960@163.com>
2019-02-12 11:38:43 +0800

    chicken更改opcontrol.cpp
    Signed-off-by: wuyunze2005 <15067000960@163.com>

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -2,5 +2,9 @@
 
 void opcontrol()
 {
-    std::cout << "hello word!" << std::endl;
+    int aaa[9] = {123, 43, 2232, 13, 231, 31, 2, 312, 3};
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout << aaa[i] << std::endl;
+    }
 }
